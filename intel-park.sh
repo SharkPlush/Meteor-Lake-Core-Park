a84cc136ba00efeae9823c7f4bac00211a21512f
SharkPlsuh <larryisbetter@proton.me>
2026-09-15 19:02:06 +0300

    Update intel-park.sh

--- intel-park.sh
+++ intel-park.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 monitor_fun() {
-    if ! STATE="$(busctl --system wait org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.DBus.Properties PropertiesChanged | grep -m1 -oE "power-saver|balanced|performance")"; then
-        printf "Failed to capture the power profile.\n"; return 1
-    else
-        printf "Power profile has changed.\n"; return 0
-    fi
+    while true; do
+        local I
+        if ! I="$(busctl --system wait org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.DBus.Properties PropertiesChanged)"; then
+            printf "Failed to start busctl listener.\n"; return 1
+        fi
+        grep -q '"ActiveProfile"' <<<$I || continue
+        if ! STATE="$(busctl --system get-property org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.UPower.PowerProfiles ActiveProfile | grep -m1 -oE "power-saver|balanced|performance")"; then
+            printf "Failed to capture power profile state.\n"; return 1
+        fi
+        break
+    done
+    return 0
 }
 
 apply_park_fun() {
@@ -21,6 +28,7 @@ apply_park_fun() {
             if ! printf '0-17\n' > $PARK_DIR/cpuset.cpus.exclusive; then
                 return 1
             fi
+            printf "Applied performance CPU adjustment.\n"
             ;;
         balanced)
             if ! printf '0,1,2,5,8,9,10,11\n' > $PARK_DIR/cpuset.cpus; then
@@ -32,6 +40,7 @@ apply_park_fun() {
             if ! printf 'isolated\n' > $PARK_DIR/cpuset.cpus.partition; then
                 return 1
             fi
+            printf "Applied balanced CPU adjustment.\n"
             ;;
         power-saver)
             if ! printf '0-13\n' > $PARK_DIR/cpuset.cpus; then
@@ -43,13 +52,15 @@ apply_park_fun() {
             if ! printf 'isolated\n' > $PARK_DIR/cpuset.cpus.partition; then
                 return 1
             fi
+            printf "Applied power saving CPU adjustment.\n"
             ;;
     esac
     return 0
 }
 
 # ----- ENTRY POINT -----
-PARK_DIR="/sys/fs/cgroup/parked-cores/"
+PARK_DIR="/sys/fs/cgroup/parked-cores"
+printf '+cpuset\n' > /sys/fs/cgroup/cgroup.subtree_control
 if ! STATE="$(busctl --system get-property org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.UPower.PowerProfiles ActiveProfile | grep -m1 -oE "power-saver|balanced|performance")"; then
     printf "Failed to capture power profile state when starting script.\n"; exit 1
 fi
