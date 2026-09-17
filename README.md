5954e7e1c71befc33f3c5138a44d21a35f26cff3
SharkPlsuh <larryisbetter@proton.me>
2026-09-17 22:07:02 +0300

    Add dynmaic CPU detection.
    While I was washing dishes and thinking because what else do you do when
    washing dishes, I figured out that adding dynamic CPU detect wouldn't be
    as hard as I thought.
    
    Also a small fix for switching to all cores.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Won't make this dynamic (unless someone wants to PR that) but it is very easy to adjust for your own CPU. In my testing I gained about 1.5 watts while watching videos.
+In my testing I gained about 1.5 watts while watching videos.
 
 I don't recommend using this on anything newer than Meteor Lake the kernel handles those CPUs differently. I also haven't tested this on older CPUs so that'd be cool if someone can test that.
--- intel-park.sh
+++ intel-park.sh
@@ -14,13 +14,13 @@ apply_park_fun() {
         fi
         printf 'P cores have been parked for '$POWER_STATE' mode.\n'
     else
+        if ! printf 'member' > /sys/fs/cgroup/parked-cores/cpuset.cpus.partition; then
+            return 1
+        fi
         if ! printf '%s' "$A_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus; then
             return 1
         fi
         if ! printf '%s' "$A_CORES" > /sys/fs/cgroup/parked-cores/cpuset.cpus.exclusive; then
-            return 1
-        fi
-        if ! printf 'member' > /sys/fs/cgroup/parked-cores/cpuset.cpus.partition; then
             return 1
         fi
         printf 'Allowing all cores for '$POWER_STATE' mode.\n'
@@ -29,12 +29,23 @@ apply_park_fun() {
 }
 
 # ----- ENTRY POINT -----
-P_CORES="0-7"
-A_CORES="0-17"
+CPU_GEN="$(awk '/^model\t/{print $3;exit}' /proc/cpuinfo)"
+
+case $CPU_GEN in
+    151|154|183|186|191|170|172)
+        printf 'Supported CPU found.\n'
+        ;;
+    *)
+        printf 'Your CPU is not supported.\n'
+        exit 2
+        ;;
+esac
+
+P_CORES="$(< /sys/devices/cpu_core/cpus)"
+A_CORES="$(< /sys/devices/system/cpu/present)"
 BUSCTL_OUT=""
 POWER_STATE=""
 trap 'POWER_STATE="performance"; apply_park_fun' EXIT
-
 
 if ! printf '+cpuset\n' > /sys/fs/cgroup/cgroup.subtree_control; then
     printf "Failed to add +cpuset to cgroup.subtree_control\n"
@@ -52,7 +63,6 @@ if ! apply_park_fun; then
      printf 'Failed to adjust parked CPU cores.\n'
      exit 1
 fi
-
 
 while true; do
     if ! BUSCTL_OUT="$(busctl --system wait org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.DBus.Properties PropertiesChanged)"; then
