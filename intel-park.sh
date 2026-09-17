e22a9a955c85db13b51b1fbbc54a14e62e03cfae
SharkPlsuh <larryisbetter@proton.me>
2026-09-17 18:36:18 +0300

    Get rid of CPU rotation
    power-saver should be allowed to have all E cores because when working
    harder having 2 E cores + 2 LPE cores can make it less power efficient
    since it takes longer to finish a task. If the device isn't working hard
    then the E cores just sleep like if they were parked anyways.
    
    The difference between balanced and performance is the CPU govener aka
    the regular GNOME / KDE power profiles.

--- intel-park.sh
+++ intel-park.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-cleanup_fun() {
-    if ! printf '0-17\n' > $PARK_DIR/cpuset.cpus; then
-        printf "Failed to put cores into cpuset.cpu during cleanup.\n"
-    fi
-    if ! printf '0-17\n' > $PARK_DIR/cpuset.cpus.exclusive; then
-        printf "Failed to put cores into cpuset.exclusive during cleanup.\n"
-    fi
-    if ! printf 'member\n' > $PARK_DIR/cpuset.cpus.partition; then
-        printf "Failed to put cores into member state during cleanup.\n"
-    fi
-}
-
 monitor_fun() {
-    local I REMAIN E_CODE=0
+    local I E_CODE=0
     while true; do
-        case $POWER_MODE in
-            1)
-                I="$(timeout 1800 busctl --system wait org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.DBus.Properties PropertiesChanged)" || E_CODE=$?
-                ;;
-            *)
-                I="$(busctl --system wait org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.DBus.Properties PropertiesChanged)" || E_CODE=$?
-                ;;
-        esac
-        if [ "$E_CODE" = "124" ]; then
-            return 0
-        fi
-        if [ "$E_CODE" -ne "0" ]; then
+        if ! I="$(busctl --system wait org.freedesktop.UPower.PowerProfiles /org/freedesktop/UPower/PowerProfiles org.freedesktop.DBus.Properties PropertiesChanged)"; then
             printf "Failed to start busctl listener.\n"
             return 1
         fi
@@ -43,117 +20,36 @@ monitor_fun() {
 
 apply_park_fun() {
     printf "Readjusting parked CPU cores.\n"
-    case $POWER_STATE in
-        balanced)
-            case $BALANCED_ROTATE in
-                0)
-                    if ! printf '0,1,2,5,8,9,10,11\n' > $PARK_DIR/cpuset.cpus; then
-                        return 1
-                    fi
-                    if ! printf '0,1,2,5,8,9,10,11\n' > $PARK_DIR/cpuset.cpus.exclusive; then
-                        return 1
-                    fi
-                    if ! printf 'isolated\n' > $PARK_DIR/cpuset.cpus.partition; then
-                        return 1
-                    fi
-                    printf "Balanced rotation 1\n"
-                    BALANCED_ROTATE="1"
-                    ;;
-                1)
-                    if ! printf '3,4,6,7,12,13,14,15\n' > $PARK_DIR/cpuset.cpus; then
-                        return 1
-                    fi
-                    if ! printf '3,4,6,7,12,13,14,15\n' > $PARK_DIR/cpuset.cpus.exclusive; then
-                        return 1
-                    fi
-                    if ! printf 'isolated\n' > $PARK_DIR/cpuset.cpus.partition; then
-                        return 1
-                    fi
-                    printf "Balanced rotation 2\n"
-                    BALANCED_ROTATE="0"
-                    ;;
-            esac
-            POWER_MODE="1"
-            ;;
-        power-saver)
-            case $POWER_SAVER_ROTATE in
-                0)
-                    if ! printf '0-7,10,11,12,13,14,15\n' > $PARK_DIR/cpuset.cpus; then
-                        return 1
-                    fi
-                    if ! printf '0-7,10,11,12,13,14,15\n' > $PARK_DIR/cpuset.cpus.exclusive; then
-                        return 1
-                    fi
-                    if ! printf 'isolated\n' > $PARK_DIR/cpuset.cpus.partition; then
-                        return 1
-                    fi
-                    printf "Power-saver rotation 1\n"
-                    POWER_SAVER_ROTATE="1"
-                    ;;
-                1)
-                    if ! printf '0-7,8,9,12,13,14,15\n' > $PARK_DIR/cpuset.cpus; then
-                        return 1
-                    fi
-                    if ! printf '0-7,8,9,12,13,14,15\n' > $PARK_DIR/cpuset.cpus.exclusive; then
-                        return 1
-                    fi
-                    if ! printf 'isolated\n' > $PARK_DIR/cpuset.cpus.partition; then
-                        return 1
-                    fi
-                    printf "Power-saver rotation 2\n"
-                    POWER_SAVER_ROTATE="2"
-                    ;;
-                2)
-                    if ! printf '0-7,8,9,10,11,14,15\n' > $PARK_DIR/cpuset.cpus; then
-                        return 1
-                    fi
-                    if ! printf '0-7,8,9,10,11,14,15\n' > $PARK_DIR/cpuset.cpus.exclusive; then
-                        return 1
-                    fi
-                    if ! printf 'isolated\n' > $PARK_DIR/cpuset.cpus.partition; then
-                        return 1
-                    fi
-                    printf "Power-saver rotation 3\n"
-                    POWER_SAVER_ROTATE="3"
-                    ;;
-                3)
-                    if ! printf '0-7,8,9,10,11,12,13\n' > $PARK_DIR/cpuset.cpus; then
-                        return 1
-                    fi
-                    if ! printf '0-7,8,9,10,11,12,13\n' > $PARK_DIR/cpuset.cpus.exclusive; then
-                        return 1
-                    fi
-                    if ! printf 'isolated\n' > $PARK_DIR/cpuset.cpus.partition; then
-                        return 1
-                    fi
-                    printf "Power-saver rotation 4\n"
-                    POWER_SAVER_ROTATE="0"
-                    ;;
-            esac
-            POWER_MODE="1"
-            ;;
-        *)
-            if ! printf '0-17\n' > $PARK_DIR/cpuset.cpus; then
-                return 1
-            fi
-            if ! printf '0-17\n' > $PARK_DIR/cpuset.cpus.exclusive; then
-                return 1
-            fi
-            if ! printf 'member\n' > $PARK_DIR/cpuset.cpus.partition; then
-                return 1
-            fi
-            POWER_MODE="0"
-            ;;
-    esac
+    if [ "$POWER_STATE" = "power-saver" ]; then
+        if ! printf '0-7\n' > $PARK_DIR/cpuset.cpus; then
+            return 1
+        fi
+        if ! printf '0-7\n' > $PARK_DIR/cpuset.cpus.exclusive; then
+            return 1
+        fi
+        if ! printf 'isolated\n' > $PARK_DIR/cpuset.cpus.partition; then
+            return 1
+        fi
+        printf "Power-saver\n"
+        ;;
+    else
+        if ! printf '0-17\n' > $PARK_DIR/cpuset.cpus; then
+            return 1
+        fi
+        if ! printf '0-17\n' > $PARK_DIR/cpuset.cpus.exclusive; then
+            return 1
+        fi
+        if ! printf 'member\n' > $PARK_DIR/cpuset.cpus.partition; then
+            return 1
+        fi
+        printf "Performance\n"
+    fi
     return 0
 }
 
 # ----- ENTRY POINT -----
-trap 'cleanup_fun'; EXIT
-POWER_MODE=""
-BALANCED_ROTATE=$(( RANDOM % 2 ))
-POWER_SAVER_ROTATE=$(( RANDOM % 4 ))
 PARK_DIR="/sys/fs/cgroup/parked-cores"
+trap 'POWER_STATE="power-saver"; apply_park_fun' EXIT
 if ! printf '+cpuset\n' > /sys/fs/cgroup/cgroup.subtree_control; then
     printf "Failed to add +cpuset to cgroup.subtree_control\n"
     exit 1
